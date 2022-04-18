9abaa5ba987a48462a2ef958e5685364643b4dfc
tamam03 <tamam0308@gmail.com>
2022-04-18 11:32:56 +0000

    migrate_reset

---
+++ db/migrate/20220418102123_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :products do |t|
+      
+      t.integer :genre_id
+      t.string :name
+      t.integer :price
+      t.text :caption
+      
+
+      t.timestamps
+      
+    end
+  end
+end
