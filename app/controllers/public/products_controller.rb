2608ddc6c53496ebf75e068b55f6b14c6c09c39f
tamam03 <tamam0308@gmail.com>
2022-04-19 06:11:39 +0000

    add public/products_view_rute

--- app/controllers/public/products_controller.rb
+++ app/controllers/public/products_controller.rb
@@ -1,2 +1,9 @@
 class Public::ProductsController < ApplicationController
+
+  def index
+  end
+
+  def show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-
-    resources :products, only: [:index, :show, :edit, :new, :create, :update]
-
-  end
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: 'public/registrations',
     sessions: 'public/sessions'
@@ -13,5 +8,15 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: 'admin/sessions'
   }
+
+
+   namespace :admin do
+    resources :products, only: [:index, :show, :edit, :new, :create, :update]
+  end
+
+  scope module: :public do
+    resources :products, only: [:index, :show]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
