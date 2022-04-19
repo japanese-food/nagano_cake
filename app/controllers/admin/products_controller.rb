12bb9feb08baaaf0fcf6617fdff5157801f2adfb
tamam03 <tamam0308@gmail.com>
2022-04-19 08:20:11 +0000

    admin/product_new

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -6,8 +6,21 @@ class Admin::ProductsController < ApplicationController
   end
 
   def new
+    @product_new = Product.new
+  end
+
+  def create
+    @product_new = Product.new(product_params)
+    @product_new.save
+    redirect_to product_path(@product)
   end
 
   def edit
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:image, :name, :caption)
   end
 end
