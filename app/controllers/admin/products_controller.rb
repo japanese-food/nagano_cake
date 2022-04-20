291a2293398f0f35b711e3bfce3133475449cf4c
tamam03 <tamam0308@gmail.com>
2022-04-20 09:59:00 +0000

    add admin/product

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -21,9 +21,15 @@ class Admin::ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def update
+    product = Product.find(params[:id])
+    product.update(product_params)
+    redirect_to admin_product_path(product.id)
+  end
+
   private
 
   def product_params
-    params.require(:product).permit(:image, :name, :caption)
+    params.require(:product).permit(:image, :name, :caption, :price, :sales_status, :genre_id)
   end
 end
