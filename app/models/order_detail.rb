c27c74bea68f3417304d2d27e21afb15ebfb5668
tamam03 <tamam0308@gmail.com>
2022-04-20 01:16:10 +0000

    make making_status

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,6 +1,5 @@
 class OrderDetail < ApplicationRecord
 
-  # enum making_method: { cannot: 0, wait: 1, making: 2, completion: 3 }
-
+    enum making_status: { cannot: 0, wait: 1, making: 2, completion: 3 }
 end
 
