f47551772b5d3ce15082fbcd8fe5fd87a74fd8c6
k898006 <kazutoshi.saito225@gmail.com>
2022-04-21 09:50:45 +0000

    add validation onto customer.rb

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,9 +3,17 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
-  
+
+
   def active_for_authentication?
     super && (is_deleted == false)
   end
+
+  validates :sur_name, presence: true
+  validates :first_name, presence: true
+  validates :sur_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :address, presence: true
+  validates :post_code, presence: true, length: {is: 6}
+  validates :phone_number, presence: true
 end
