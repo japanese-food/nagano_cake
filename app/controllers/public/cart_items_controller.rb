026e02bf3bf25cd0d5ddd3ca4ca2e730d1effc52
tamam03 <tamam0308@gmail.com>
2022-04-21 10:02:12 +0000

    price

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,13 +1,15 @@
 class Public::CartItemsController < ApplicationController
   def index
     @product =  Product.find_by(params[:product_id])
-    @cart_items = @product.cart_items
+    @cart_items = current_customer.cart_items
+    @total_price = 0
   end
 
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
     @cart_item.save
+    redirect_to cart_items_path
 
   end
 
