b957c2f7d9c3b64fdfdeed431d531ef28536e92a
k898006 <kazutoshi.saito225@gmail.com>
2022-04-21 09:36:02 +0000

    complete admin/customers index, show, edit, update

---
+++ app/controllers/admin/customers_controller.rb
@@ -0,0 +1,26 @@
+class Admin::CustomersController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path(@customer)
+  end
+
+ private
+
+  def customer_params
+    params.require(:customer).permit(:email, :sur_name, :first_name, :sur_name_kana, :first_name_kana, :post_code, :address, :phone_number, :is_deleted)
+  end
+
+end
