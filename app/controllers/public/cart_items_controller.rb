7d792323f45e1d946181d0414179d26ac35ed5ff
tamam03 <tamam0308@gmail.com>
2022-04-23 12:55:02 +0000

    update

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -8,9 +8,16 @@ class Public::CartItemsController < ApplicationController
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
+    if cart_item = current_customer.cart_items.find_by(product_id: params[:cart_item][:product_id])
+       cart_item.quantity += params[:cart_item][:quantity].to_i
+      # new_quantity = cart_item.quantity + @cart_item.quantity
+      # cart_item.update_attribute(:quantity, new_quantity)
+      cart_item.save
+      redirect_to cart_items_path
+    else
     @cart_item.save
     redirect_to cart_items_path
-
+    end
   end
 
   def destroy
@@ -25,6 +32,14 @@ class Public::CartItemsController < ApplicationController
 
   def destroy_all
     @cart_item = CartItem.destroy_all
+    redirect_to  request.referer
+  end
+
+  def update
+    @cart_iteme = CartItem.find_by(params[:quantity])
+    # new_quantity = @cart_item.quantity
+    # @cart_item.update_attribute(:quantity, new_quantity)
+    # @cart_iteme.quantity.update
     redirect_to  request.referer
   end
 
