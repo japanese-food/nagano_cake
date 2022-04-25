2f815c5570437ec5dab635c93e0991911b6c060a
tamam03 <tamam0308@gmail.com>
2022-04-25 02:20:33 +0000

    genre

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -9,6 +9,7 @@ class Admin::ProductsController < ApplicationController
 
   def new
     @product_new = Product.new
+    @genres = Genre.all
   end
 
   def create
@@ -19,6 +20,7 @@ class Admin::ProductsController < ApplicationController
 
   def edit
     @product = Product.find(params[:id])
+    @genres = Genre.all
   end
 
   def update
