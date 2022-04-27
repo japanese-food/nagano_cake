496210f2876687bf86da7f0235db27297c048a05
tamam03 <tamam0308@gmail.com>
2022-04-27 11:53:15 +0000

    price

--- app/models/product.rb
+++ app/models/product.rb
@@ -14,7 +14,7 @@ class Product < ApplicationRecord
       file_path = Rails.root.join('app/assets/images/no-image.png')
       image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
-     image.variant(resize_to_limit: [width, height]).processed
+    image.variant(resize_to_limit: [width, height]).processed
     end
 
 
