01099c9dad2f55ba73712caea1d42a68b0f5a92b
k898006 <kazutoshi.saito225@gmail.com>
2022-04-27 04:02:22 +0000

    complete admin/oreder_details/update

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -2,11 +2,9 @@ class Admin::OrderDetailsController < ApplicationController
 
 
   def update
-     @order = Order.find(params[:id])
-     @order_details = @order.order_details
-     @order_details.update(order_detail_params)
-    # @order.update(order_params)
-       redirect_to admin_order_path(@order)
+    @order_details = OrderDetail.find(params[:id])
+    @order_details.update(order_detail_params)
+    redirect_to request.referer
   end
 
 
