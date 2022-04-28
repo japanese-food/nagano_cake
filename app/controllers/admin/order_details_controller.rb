99ad392058acb0540e818670e56d78cac55a96af
k898006 <kazutoshi.saito225@gmail.com>
2022-04-28 05:55:28 +0000

    add order_details update action

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,7 +3,11 @@ class Admin::OrderDetailsController < ApplicationController
 
   def update
     @order_details = OrderDetail.find(params[:id])
+    @order = @order_details.order
     @order_details.update(order_detail_params)
+    if @order.order_details.count == @order.order_details.where(making_status: "completion").count
+    @order.update(status: "preparation")
+    end
     redirect_to request.referer
   end
 
