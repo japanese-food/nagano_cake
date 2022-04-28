98763c9192c6384a75871c26f7111239ac346f82
k898006 <kazutoshi.saito225@gmail.com>
2022-04-28 05:12:52 +0000

    add validation onto cart_item.rb

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -7,15 +7,18 @@ class Public::CartItemsController < ApplicationController
   def create
     @cart_item = CartItem.new(cart_item_params)
     @cart_item.customer_id = current_customer.id
-    if cart_item = current_customer.cart_items.find_by(product_id: params[:cart_item][:product_id])
+    cart_item = CartItem.find_by(product_id: params[:cart_item][:product_id], customer_id: current_customer.id)
+    if cart_item.present?
        cart_item.quantity += params[:cart_item][:quantity].to_i
       # new_quantity = cart_item.quantity + @cart_item.quantity
       # cart_item.update_attribute(:quantity, new_quantity)
       cart_item.save
       redirect_to cart_items_path
-    else
+    elsif
     @cart_item.save
     redirect_to cart_items_path
+    else
+    redirect_to products_path
     end
   end
 
